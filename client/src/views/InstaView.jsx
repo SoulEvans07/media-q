c9700d09b3a96de187da824f8104c64daf31272a
Soul Evans <szi.adam@simonyi.bme.hu>
2020-01-16 17:16:51 +0100

    [client] basic daily story fetch

--- client/src/views/InstaView.jsx
+++ client/src/views/InstaView.jsx
@@ -1,14 +1,53 @@
 import React, { Component } from 'react'
 import './InstaView.scss'
 
-const targetFolder = '/../../server/target'
+import Loader from '../components/Loader'
 
 class InstaView extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      stories: null
+    }
+  }
+
+  componentDidMount() {
+    const url = 'http://localhost:3000/api/insta/story/list/2020-01-16'
+
+    fetch(url)
+    .then(res => res.json())
+    .then(res => {
+      if (res.error) {
+        this.setState({ stories: [] })
+        console.error(res.error)
+      } else {
+        this.setState({ stories: Object.values(res) })
+      }
+    })
+  }
 
   render() {
+    const URL_BASE = 'http://localhost:3000/api/insta/story/'
+    const { stories } = this.state
+
     return (
       <div id="insta-view">
-
+        { stories ?
+          <div className="story-list">
+            { stories.map(story => (
+              <span className="story-card" key={ story.src }>
+                <a href={ URL_BASE + story.src }>
+                  <img className="story-thumbnail" src={ URL_BASE + story.thumbnail } alt={ story.src } />
+                </a>
+              </span>
+            ))}
+          </div>
+          :
+          <div className="loading">
+            <Loader />
+          </div>
+        }
       </div>
     )
   }
